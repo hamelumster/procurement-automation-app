3a3358f04f24d46a04e73c360b157b5309e70303
Nicki tst <kanieleoutiz@gmail.com>
2025-07-05 03:10:49 +0300

    add DeliveryContact

--- users/models.py
+++ users/models.py
@@ -28,3 +28,25 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.email} ({self.get_role_display()})"
 
+
+class DeliveryContact(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='delivery_contact'
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    house = models.CharField(max_length=100)
+    building = models.CharField(max_length=100, blank=True, null=True)
+    apartment = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.city}, {self.street}, {self.house}"
