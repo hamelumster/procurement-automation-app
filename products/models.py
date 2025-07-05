2062dff93c1232826cc42cb2732c88faefa9000b
Nicki tst <kanieleoutiz@gmail.com>
2025-07-05 03:48:42 +0300

    add Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    supplier = models.ForeignKey(
+        'users.SupplierProfile',
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    characteristics = models.JSONField(
+        blank=True,
+        null=True,
+        help_text='Характеристики товара в виде словаря'
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.supplier.user.email})"
+    
