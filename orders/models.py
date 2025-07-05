1bdb75e41a1d2329718899167b55322f7453d456
Nicki tst <kanieleoutiz@gmail.com>
2025-07-05 04:07:00 +0300

    add Cart

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Cart(models.Model):
+    """
+    Корзина текущих товаров у пользователя
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='carts'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart #{self.id} {self.user.email}"
+
+
