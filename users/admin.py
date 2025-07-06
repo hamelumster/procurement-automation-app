b8b9cd4c1ebaf8f3e8b9a5a80b24a8840302e36b
Nicki tst <kanieleoutiz@gmail.com>
2025-07-06 13:40:49 +0300

    add UserProfileAdmin and SupplierProfileAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import UserProfile, SupplierProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+
+@admin.register(SupplierProfile)
+class SupplierProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
