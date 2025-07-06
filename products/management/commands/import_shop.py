1e248bf09b5cac476981571491ed434e82a39c0f
Nicki tst <kanieleoutiz@gmail.com>
2025-07-06 11:56:04 +0300

    add handle(): add load .yaml

--- products/management/commands/import_shop.py
+++ products/management/commands/import_shop.py
@@ -1,4 +1,5 @@
-from django.core.management import BaseCommand
+import yaml
+from django.core.management import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
@@ -8,3 +9,16 @@ class Command(BaseCommand):
                             type=str,
                             help='Путь к yaml-файлу с данными'
         )
+
+    def handle(self, *args, **options):
+        yaml_path = options['yaml_file']
+
+        # 1 Загрузить yaml файл
+        try:
+            with open(yaml_path, 'r', encoding='utf-8') as f:
+                data = yaml.safe_load(f)
+        except FileNotFoundError:
+            raise CommandError(f'Файл {yaml_path} не найден')
+        except yaml.YAMLError as e:
+            raise CommandError(f'Ошибка: {e}')
+        
