2dc4590bf60bbadc0e72c6ad3c1447bcefef467b
Nicki tst <kanieleoutiz@gmail.com>
2025-07-06 11:43:01 +0300

    add add_argument() in Command

--- products/management/commands/import_shop.py
+++ products/management/commands/import_shop.py
@@ -2,4 +2,9 @@ from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
-    pass
+    help="Импортирует товары из yaml файла"
+    def add_arguments(self, parser):
+        parser.add_argument('yaml_file',
+                            type=str,
+                            help='Путь к yaml-файлу с данными'
+        )
