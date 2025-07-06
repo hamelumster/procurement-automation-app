3de973d277c8a32202186ad0721fe7ba876f3cbc
Nicki tst <kanieleoutiz@gmail.com>
2025-07-06 12:20:17 +0300

    add creating or getting shop from .yaml

--- products/management/commands/import_shop.py
+++ products/management/commands/import_shop.py
@@ -33,9 +33,21 @@ class Command(BaseCommand):
         except yaml.YAMLError as e:
             raise CommandError(f'Ошибка: {e}')
 
-        # Найти поставщика по email
+        # 2 Найти поставщика по email
         try:
             supplier = SupplierProfile.objects.get(user__email=supplier_email)
         except SupplierProfile.DoesNotExist:
             raise CommandError(f'Поставщик с email {supplier_email} не найден')
+
+        # 3 Создать или получить магазин по имени из yaml файла
+        shop_name = data.get('shop')
+        if not shop_name:
+            raise CommandError('Название магазина не указано в yaml файле')
+        shop, created = Shop.objects.get_or_create(
+            name=shop_name,
+            supplier=supplier,
+            defaults={'description': shop_name, 'is_active': True}
+        )
+        self.stdout.write(f"Магазин {shop_name} {'создан' if created else 'найден'}")
+
         
