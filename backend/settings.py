acd423fb866892ea8127677e2984a342e00bb0f7
Nicki tst <kanieleoutiz@gmail.com>
2025-07-10 07:57:22 +0300

    add rest_framework.permissions.IsAuthenticated in REST_FRAMEWORK

--- backend/settings.py
+++ backend/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
