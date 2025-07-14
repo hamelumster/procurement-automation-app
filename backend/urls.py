9bcaca62c8961a68170a4d97de0adb92a6b78be6
Nicki tst <kanieleoutiz@gmail.com>
2025-07-15 00:26:13 +0300

    add path api/auth/register/

--- backend/urls.py
+++ backend/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.views import RegisterAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/register/', RegisterAPIView.as_view(), name='auth_register'),
 ]
