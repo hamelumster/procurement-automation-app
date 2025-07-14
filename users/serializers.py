afa9d03c96d2843218d5348bab0d6cf3d532507e
Nicki tst <kanieleoutiz@gmail.com>
2025-07-14 23:41:58 +0300

    add RegistrationSerializer and fill the fields

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,16 @@
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password')
+        
