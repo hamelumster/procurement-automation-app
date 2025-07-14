1816f4eddd4a4bc4914e1d4f423f2ee2ad8f855b
Nicki tst <kanieleoutiz@gmail.com>
2025-07-15 02:22:57 +0300

    add EmailTokenObtainPairView(TokenObtainPairView)

--- users/views.py
+++ users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import permissions, status, generics
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from users.serializers import RegistrationSerializer
+from users.serializers import RegistrationSerializer, EmailTokenObtainPairSerializer
 
 
 class RegisterAPIView(generics.CreateAPIView):
@@ -44,3 +45,10 @@ class RegisterAPIView(generics.CreateAPIView):
             status=status.HTTP_201_CREATED
         )
 
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+    """
+    Логин по email + password;
+    возвращает refresh и access
+    """
+    serializer_class = EmailTokenObtainPairSerializer
