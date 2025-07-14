e14d4489fc86a3f158e31e6ab191dbd8dc230797
Nicki tst <kanieleoutiz@gmail.com>
2025-07-15 00:36:48 +0300

    add test_register(): registration

--- users/tests/test_auth.py
+++ users/tests/test_auth.py
@@ -1,8 +1,25 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 User = get_user_model()
 
 
 class AuthTests(APITestCase):
-    pass
+
+    def test_register(self):
+        url_register = reverse('auth_register')
+        data = {
+            'first_name': 'Jack',
+            'last_name': 'Sparrow',
+            'email': 'black_pearl@example.com',
+            'password': '12345678'
+        }
+
+        # Регистрация
+        response = self.client.post(url_register, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+        
