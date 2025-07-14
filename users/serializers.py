fa6eda4117ba8cb1fcd748004d20931b2e0f285b
Nicki tst <kanieleoutiz@gmail.com>
2025-07-14 23:43:56 +0300

    add validate_email

--- users/serializers.py
+++ users/serializers.py
@@ -13,4 +13,11 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'password')
-        
+
+    def validate_email(self, value):
+        """Проверка уникальности email"""
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Пользователь с таким email уже зарегистрирован')
+        return value
+
+    
