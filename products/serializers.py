43055ca5d274f413c6a5b75de45236f718364182
Nicki tst <kanieleoutiz@gmail.com>
2025-07-17 20:54:09 +0300

    add ProductSerializer

---
+++ products/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from products.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    supplier = serializers.CharField(source='shop.name', read_only=True)
+    category = serializers.CharField(source='category.name', read_only=True)
+
+    class Meta:
+        model = Product
+        fields = [
+            'external_id',
+            'id',
+            'name',
+            'description',
+            'supplier',
+            'category',
+            'characteristics',
+            'price',
+            'price_rrc',
+            'quantity',
+        ]
