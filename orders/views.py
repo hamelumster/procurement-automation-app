cfeb8532dd1c675b03de4f7e3eafc080b8138cfb
Nicki tst <kanieleoutiz@gmail.com>
2025-07-18 09:33:06 +0300

    add CartViewSet

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class CartViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+
