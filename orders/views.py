4cdd840a155297d7f008c6278cbf880a9a003c4b
Nicki tst <kanieleoutiz@gmail.com>
2025-07-18 09:35:34 +0300

    add list() in CartViewSet

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from orders.models import Cart
+from orders.serializers import CartSerializer
 
 
 class CartViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
 
-
+    def list(self, request):
+        """
+        GET /api/cart/
+        -> возвращает текущую корзину пользователя
+        """
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
