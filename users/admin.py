f954424c6e69d314e0473f8c745965450e27787d
Nicki tst <kanieleoutiz@gmail.com>
2025-07-29 21:09:10 +0300

    add 'image' supplier status

--- users/admin.py
+++ users/admin.py
@@ -5,8 +5,14 @@ from users.models import UserProfile, SupplierProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role')
+    list_display = ('user', 'is_supplier', 'created_at', 'updated_at')
+    list_select_related = ('user',)
+
+    def is_supplier(self, obj):
+        return hasattr(obj.user, 'supplier_profile')
+
+    is_supplier.boolean = True
 
 @admin.register(SupplierProfile)
 class SupplierProfileAdmin(admin.ModelAdmin):
-    list_display = ('user',)
+    list_display = ('user', )
