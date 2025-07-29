be5a43afa59df6265d6dc3bc1a4b8d438b12ad6f
Nicki tst <kanieleoutiz@gmail.com>
2025-07-30 01:03:13 +0300

    add _get_or_create_shop()

--- shops/services/shop_import.py
+++ shops/services/shop_import.py
@@ -1,3 +1,4 @@
+from shops.models import Shop
 from users.models import SupplierProfile
 
 
@@ -7,3 +8,15 @@ class ShopImportService:
         self.shop_data = shop_data
         self.created_cats = self.updated_cats = 0
         self.created_products = self.updated_products = 0
+
+    def _get_or_create_shop(self):
+        name = self.shop_data.get('shop')
+        if not name:
+            raise ValueError('Название магазина не указано в yaml файле')
+        shop, created = Shop.objects.get_or_create(
+            name=name,
+            supplier=self.supplier,
+            defaults={'description': name, 'is_active': True}
+        )
+        return shop
+
