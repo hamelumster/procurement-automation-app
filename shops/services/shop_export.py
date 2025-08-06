9cf9e66b746807bb5a30caadcffa6a9338a50757
Nicki tst <kanieleoutiz@gmail.com>
2025-08-06 17:05:31 +0300

    add assembly_categories(), assembly_products(), run()

--- shops/services/shop_export.py
+++ shops/services/shop_export.py
@@ -1,3 +1,4 @@
+from shops.models import Shop
 
 
 class ShopExportService:
@@ -5,4 +6,14 @@ class ShopExportService:
     Собирает для заданного магазина товары,
     и возвращает валидный YAML-документ в виде строки.
     """
-    pass
+    def __init__(self, shop: Shop):
+        self.shop = Shop
+
+    def assembly_categories(self):
+        pass
+
+    def assembly_products(self):
+        pass
+
+    def run(self):
+        pass
